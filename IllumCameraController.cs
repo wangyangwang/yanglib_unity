0333a8d58c4fa8dcea9052fcb0f96bd2c74ec272
Yang Wang <yang.wang.dec@gmail.com>
2021-01-28 17:20:55 -0500

    added pano cam script

--- IllumCameraController.cs
+++ IllumCameraController.cs
@@ -1,17 +1,20 @@
 ﻿using UnityEngine;
 
+using UnityEditor;
+
 [ExecuteInEditMode]
 public class IllumCameraController : MonoBehaviour
 {
     [System.Serializable]
-    public struct CamInfo
+    public struct Cam
     {
         public string name;
-        public Vector3 rotation;
+        public Camera cam;
     }
 
-    public CamInfo[] presets;
-    string currentCamPreset;
+    public Cam[] cameras;
+    string activeCam;
+    public bool showControl;
 
     // Start is called before the first frame update
     void Start()
@@ -26,30 +29,33 @@ public class IllumCameraController : MonoBehaviour
 
     }
 
+
+
     void OnGUI()
     {
+        if (!showControl) return;
         int buttonHeight = Screen.height / 20;
         int buttonWidth = Screen.width / 10;
 
-        for (int i = 0; i < presets.Length; i++)
+        for (int i = 0; i < cameras.Length; i++)
         {
-            if (GUI.Button(new Rect(0, i * buttonHeight, buttonWidth, buttonHeight), presets[i].name))
+            if (GUI.Button(new Rect(0, i * buttonHeight, buttonWidth, buttonHeight), cameras[i].name))
             {
-                transform.rotation = Quaternion.Euler(presets[i].rotation);
-                currentCamPreset = presets[i].name;
+                foreach (var c in cameras) { c.cam.gameObject.SetActive(false); }
+                cameras[i].cam.gameObject.SetActive(true);
+                activeCam = cameras[i].name;
             }
         }
 
-        if (GUI.Button(new Rect(0, presets.Length * buttonHeight, buttonWidth, buttonHeight), "Capture Screenshot"))
+        if (GUI.Button(new Rect(0, cameras.Length * buttonHeight, buttonWidth, buttonHeight), "Capture Screenshot"))
         {
-
-            CaptureScreenshot(currentCamPreset);
+            CaptureScreenshot(activeCam);
         }
     }
 
 
 
-    void CaptureScreenshot(string additionalString)
+    public void CaptureScreenshot(string additionalString)
     {
         string filename = System.DateTime.Now.ToString();
         filename = filename.Replace("/", "_");
@@ -57,5 +63,23 @@ public class IllumCameraController : MonoBehaviour
         filename = filename.Replace(":", "_");
         int superSize = Screen.width < 3000 ? 2 : 1;
         ScreenCapture.CaptureScreenshot("C:/Users/YW/Desktop/" + filename + "__" + additionalString + ".png", superSize);
+    }
+}
+
+
+
+
+[CustomEditor(typeof(IllumCameraController))]
+public class IllumCameraControllerEditor : Editor
+{
+
+    public override void OnInspectorGUI()
+    {
+        base.OnInspectorGUI();
+        var ta = target as IllumCameraController;
+        if (GUILayout.Button("capture"))
+        {
+            ta.CaptureScreenshot("");
+        }
     }
 }
---
+++ PanoCamScript.cs
@@ -0,0 +1,72 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEditor;
+#if UNITY_EDITOR
+[ExecuteInEditMode]
+public class PanoCamScript : MonoBehaviour
+{
+
+    public int camCount = 16;
+
+    void Update()
+    {
+        Init();
+    }
+
+    public void Init()
+    {
+
+        if (transform.childCount == 0)
+        {
+            for (int i = 0; i < camCount; i++)
+            {
+                GameObject go = new GameObject();
+                go.transform.parent = transform;
+                go.transform.localPosition = new Vector3(0, 0, 0);
+                go.name = "subCam" + i;
+                go.AddComponent<Camera>();
+            }
+        }
+        else if (transform.childCount != camCount)
+        {
+            Debug.LogError("to have the new cam count, manually delete all child cameras!");
+        }
+
+        // update all cameras
+
+        prepSubCameras();
+
+    }
+
+    void prepSubCameras()
+    {
+        float hwratio = (float)Screen.height / (float)Screen.width * (float)transform.childCount;
+        Debug.LogFormat("Screen height {0}, ScreenWidth {1}, child Cound {2}, hwratio {3}", Screen.height, Screen.width, transform.childCount, hwratio);
+        for (int i = 0; i < transform.childCount; i++)
+        {
+            Transform child = transform.GetChild(i);
+            var cam = child.GetComponent<Camera>();
+            float onepart = 1.0f / transform.childCount;
+            cam.fieldOfView = 2 * Mathf.Atan(hwratio * Mathf.Tan(180 * Mathf.Deg2Rad / transform.childCount)) * Mathf.Rad2Deg;
+            //change rotation
+            child.localEulerAngles = new Vector3(0, 360.0f / transform.childCount * i, 0);
+            //viewport rect
+            cam.rect = new Rect(onepart * i, 0, onepart, 1);
+        }
+    }
+}
+
+
+[CustomEditor(typeof(PanoCamScript))]
+class PanoCamEditor : Editor
+{
+    public override void OnInspectorGUI()
+    {
+        var script = (PanoCamScript)target;
+        base.OnInspectorGUI();
+
+
+
+    }
+}
+#endif
