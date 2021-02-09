0506b5983fc3aac38f605e86c62a9f9a878349ec
Yang Wang <yang.wang.dec@gmail.com>
2021-02-09 17:45:31 -0500

    added showHierachy on screen script

---
+++ ShowHierachyOnScreen.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[ExecuteInEditMode]
+public class ShowHierachyOnScreen : MonoBehaviour
+{
+
+    string hierachy = "";
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+      
+        
+        
+    }
+
+    void OnDrawGizmos()
+    {
+
+   
+
+        var o = UnityEditor.Selection.activeTransform;
+        if (o == null  || o.GetComponent<Renderer>()==null) return;
+        hierachy = o.name + "\n";
+
+        string prefix = "-";
+        while (o.parent != null)
+        {
+            hierachy += prefix + o.parent.name + "\n";
+            o = o.parent;
+            prefix += "-";
+        }
+
+
+        UnityEditor.Handles.Label(UnityEditor.Selection.activeTransform.position,hierachy);
+    }
+
+
+    private void OnGUI()
+    {
+       
+    }
+}
